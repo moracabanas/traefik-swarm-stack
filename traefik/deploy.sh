6e758b8cf8609f850d676e0a3ba2cdd9e5bafbf0
moracabanas <moracabanas@live.com>
2021-01-02 06:09:13 +0100

    Fixed script step description

--- traefik/deploy.sh
+++ traefik/deploy.sh
@@ -4,7 +4,7 @@
 docker network inspect traefik-public >/dev/null 2>&1 || \
     docker network create --driver=overlay traefik-public
 
-# Create a label in this node, so that the required databases used by Swarmpit is always deployed to the same node and uses the existing volume:
+# Create a label in this node, so that the required volume used by Traefik is always deployed to the same node and uses the existing volume:
 NODE_ID=$(docker info -f '{{.Swarm.NodeID}}')
 docker node update --label-add traefik-public.traefik-public-certificates=true $NODE_ID
 
